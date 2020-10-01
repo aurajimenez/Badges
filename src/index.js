20c1fff7c263981f1d0e623d48c220524d09ccf2
Aura Jimenez <auracristina.jimenezg@gmail.com>
2020-09-30 19:10:46 -0500

    Enlazando eventos a un form

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import './global.css';
-import Badge from './components/Badge';
+import BadgeNew from './pages/BadgeNew';
 
 const container = document.getElementById('root');
-ReactDOM.render(<Badge firstName="Lilly" lastName="Juli" jobtitle="Front" avatar="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"/>, container);
+ReactDOM.render(<BadgeNew />, container);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
