cfbdbe8072eb8121376ad04ab5f25e94d6b5cb62
Aura Jimenez <auracristina.jimenezg@gmail.com>
2020-10-01 16:30:20 -0500

    Routes, home y 404

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
+
 import './global.css';
-import BadgeNew from './pages/BadgeNew';
+import App from './App';
 
 const container = document.getElementById('root');
-ReactDOM.render(<BadgeNew />, container);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<App />, container);
